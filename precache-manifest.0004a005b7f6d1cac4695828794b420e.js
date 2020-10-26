94deb213e4cc38fb902551a8d72fa164dca1e789
Alessandra Prado <alessandra.prado01@fatec.sp.gov.br>
2020-10-26 14:48:47 -0300

    Updates

---
+++ precache-manifest.0004a005b7f6d1cac4695828794b420e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902d97d72bd782efdf480bce7d8377de",
+    "url": "/primeiroprojeto/index.html"
+  },
+  {
+    "revision": "1136924db68d96e80a54",
+    "url": "/primeiroprojeto/static/css/main.5940f70c.chunk.css"
+  },
+  {
+    "revision": "accc679ea15bf0efd848",
+    "url": "/primeiroprojeto/static/js/2.cd26ce23.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/primeiroprojeto/static/js/2.cd26ce23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1136924db68d96e80a54",
+    "url": "/primeiroprojeto/static/js/main.03e19cc0.chunk.js"
+  },
+  {
+    "revision": "1cabe490c281e0cf1863",
+    "url": "/primeiroprojeto/static/js/runtime-main.ac8cdb09.js"
+  }
+]);
